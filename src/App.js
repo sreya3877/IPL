c3535b3b3b4ebadf3b102922c517263f22995699
sreya3877 <sreya3877@gmail.com>
2026-02-05 11:25:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import {Switch, Route} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
+
 import Home from './components/Home'
-import TeamMatches from './components/TeamMatches'
 import NotFound from './components/NotFound'
+import TeamMatches from './components/TeamMatches'
+
 import './App.css'
 
 const App = () => (
-  <Switch>
+   <Switch>
     <Route exact path="/" component={Home} />
-    <Route exact path="/team-matches/:id" component={TeamMatches} />
+    <Route path="/team-matches/:id" component={TeamMatches} />
     <Route component={NotFound} />
   </Switch>
 )
